982ef9308551937764fa030fad7c2b92c3622598
rafaarodriguesz <rr65887@gmail.com>
2025-05-06 19:09:10 -0300

    Commit com a função de cadastrar funcionando!

---
+++ Banco de Dados/script.sql
@@ -0,0 +1,11 @@
+create database projetoIndividual;
+use projetoIndividual;
+
+create table cadastro(
+	nome varchar(45),
+    email varchar(45),
+    senha varchar(45),
+    confirmarSenha varchar(45)
+);
+
+select * from cadastro;
