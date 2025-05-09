b37bfd3395b48b2477401c9b47809347510cb9ad
rafaarodriguesz2 <rafael.arodrigues@sptech.school>
2025-05-08 22:48:02 -0300

    enviando mudanças no sistema de compara precos, adicionando paginas e valor do salario

---
+++ Banco de Dados/certo.sql
@@ -0,0 +1,16 @@
+create database projetoIndividual;
+use projetoIndividual;
+
+create table usuario(
+	id int not null primary key auto_increment,
+	nome varchar(45),
+    email varchar(45),
+    senha varchar(45)
+);
+
+select * from usuario;
+
+truncate table usuario;
+
+drop database projetoIndividual;
+
